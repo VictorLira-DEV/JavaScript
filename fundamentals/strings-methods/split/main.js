71d85f9a490692497dfcf641b425ee4309c3f7e2
VictorLira-DEV <victorliradev@gmail.com>
2021-09-08 17:16:42 -0300

    string

--- fundamentals/strings-methods/split/main.js
+++ fundamentals/strings-methods/split/main.js
@@ -1,22 +1,29 @@
 //split
 //ira retornar um array
-console.log('a+casa+do+vizinho'.split('+'));
-console.log('a casa do vizinho'.split(' '));
+// console.log('a+casa+do+vizinho'.split('+'));
+// console.log('a casa do vizinho'.split(' '));
 
-const [firstName, LastName] = `Victor Lira`.split(' ')
-console.log(firstName)
+// const [firstName, LastName] = `Victor Lira`.split(' ')
+// console.log(firstName)
 
 
-//----------------------------------------
+// //----------------------------------------
 
-const capitalizeName = function (name) {
-    const names = name.split(' ');
-    const namesUpper = [];
-    for (const n of names){
-        namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    }
-    console.log(namesUpper.join(' '));
+// const capitalizeName = function (name) {
+//     const names = name.split(' ');
+//     const namesUpper = [];
+//     for (const n of names){
+//         namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     }
+//     console.log(namesUpper.join(' '));
+// }
+
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmediun');
+
+const names = 'username     casa adfasdf    asdf'.split(' ');
+
+for(const n of names){
+    console.log(n.trim())
 }
 
-capitalizeName('jessica ann smith davis');
-capitalizeName('jonas schmediun');
--- fundamentals/strings/working-with-strings-2/main.js
+++ fundamentals/strings/working-with-strings-2/main.js
@@ -1,76 +1,89 @@
-'use strict'
-/*a string se comporta de forma parecida com array
-podemos acessar cada letra se baseando no index,
-inclusive podemos usar alguns métodos
-*/
-const airline = 'Tap Air Portugal';
+// 'use strict'
+// /*a string se comporta de forma parecida com array
+// podemos acessar cada letra se baseando no index,
+// inclusive podemos usar alguns métodos
+// */
+// const airline = 'Tap Air Portugal';
 
-// console.log(airline.toLowerCase());
-// console.log('jonas'.toUpperCase());
+// // console.log(airline.toLowerCase());
+// // console.log('jonas'.toUpperCase());
 
-//FIX  CAPITALIZATION IN NAME
-const passanger = 'JoNas';
-const passangerLower = passanger.toLowerCase();
-const passangerCorrect = passangerLower[0].toUpperCase() + passangerLower.slice(1);
+// //FIX  CAPITALIZATION IN NAME
+// const passanger = 'JoNas';
+// const passangerLower = passanger.toLowerCase();
+// const passangerCorrect = passangerLower[0].toUpperCase() + passangerLower.slice(1);
 
-// console.log(passangerCorrect)
+// // console.log(passangerCorrect)
 
-//------------------------------------------------
+// //------------------------------------------------
 
-function fixCapitalization(name) {
-    const toLower = name.toLowerCase()
-    const correct = toLower[0].toUpperCase() + toLower.slice(1)
-    // console.log(correct)
-}
+// function fixCapitalization(name) {
+//     const toLower = name.toLowerCase()
+//     const correct = toLower[0].toUpperCase() + toLower.slice(1)
+//     // console.log(correct)
+// }
 
-fixCapitalization('ViCtOr');
-fixCapitalization('victor');
-fixCapitalization('VICTOR');
-fixCapitalization('vICTOR');
-fixCapitalization('victor');
+// fixCapitalization('ViCtOr');
+// fixCapitalization('victor');
+// fixCapitalization('VICTOR');
+// fixCapitalization('vICTOR');
+// fixCapitalization('victor');
 
-//-----------------------------------------------
-//FIXING EMAIL
+// //-----------------------------------------------
+// //FIXING EMAIL
 
-const email = 'hello@jonas.io';
-const loginEmail = '  Hello@jonas.io \n'
-console.log(loginEmail)
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@jonas.io \n'
+// console.log(loginEmail)
 
-const normalizedEmail = loginEmail.toLowerCase().trim();
+// const normalizedEmail = loginEmail.toLowerCase().trim();
 
-console.log(email === normalizedEmail);
+// console.log(email === normalizedEmail);
 
-//replacing
-const priceGB = '288,97&';
-const priceUS = priceGB.replace('&', '$').replace(',', '.');
-console.log(priceGB);
-console.log(priceUS);
-const announcement = 'All passangers come to boarding door 23. boarding door 23!'
+// //replacing
+// const priceGB = '288,97&';
+// const priceUS = priceGB.replace('&', '$').replace(',', '.');
+// console.log(priceGB);
+// console.log(priceUS);
+// const announcement = 'All passangers come to boarding door 23. boarding door 23!'
 
-console.log(announcement.replace('door', 'gate')) // apenas replace na primeira ocorrencia
-console.log(announcement.replaceAll('door', 'gate')) //replace everyone
+// console.log(announcement.replace('door', 'gate')) // apenas replace na primeira ocorrencia
+// console.log(announcement.replaceAll('door', 'gate')) //replace everyone
 
-//boolen 
-const plane = 'A320neo';
-console.log(plane.includes('A320'));
-console.log(plane.includes('Boing'));
-console.log(plane.startsWith('Air'));
+// //boolen 
+// const plane = 'A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boing'));
+// console.log(plane.startsWith('Air'));
 
-if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
-    console.log('Part of the New Airbus family')
-}
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//     console.log('Part of the New Airbus family')
+// }
 
-// pratice exercise 
-// when you receive something from the user always put it to lowercase
-const checkBaggage = function (items) {
-    const baggage = items.toLowerCase('');
-    if (baggage.includes('knife') || baggage.includes('gun')) {
-        console.log('you are not allowed on board')
-    } else {
-        console.log('welcome aboard')
-    }
-}
+// // pratice exercise 
+// // when you receive something from the user always put it to lowercase
+// const checkBaggage = function (items) {
+//     const baggage = items.toLowerCase('');
+//     if (baggage.includes('knife') || baggage.includes('gun')) {
+//         console.log('you are not allowed on board')
+//     } else {
+//         console.log('welcome aboard')
+//     }
+// }
 
-checkBaggage('I have a leptop, some of Food and a pocket knife');
-checkBaggage('socks and camera');
-checkBaggage('Got some snacks and gun for protection')
+// checkBaggage('I have a leptop, some of Food and a pocket knife');
+// checkBaggage('socks and camera');
+// checkBaggage('Got some snacks and gun for protection')
+
+
+
+
+
+const name = '  victor                   dos     santos    lira';
+const names = name.replaceAll(/\s+/g, '+');
+const newName = names.split('+')
+
+console.log(newName.join(' ').trim())
+
+// for(const n of names){
+// }
