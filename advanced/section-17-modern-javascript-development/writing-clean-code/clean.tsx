3c36e0f889c3be25e6a9b99b63b28457bf07802c
VictorLira-DEV <victorliradev@gmail.com>
2021-11-30 21:08:08 -0200

    good practices

---
+++ advanced/section-17-modern-javascript-development/writing-clean-code/clean.tsx
@@ -0,0 +1,40 @@
+//READABLE CODE
+//write code so that other people can understand it 
+//write code so you can understand it in one year
+//avoid, too clever and overcomplicated solutions
+//use descriptive variables names: what they contain
+//use descriptive functions names: what they do
+
+
+
+//GENERAL
+//use DRY principle ( refactor your code)
+//don't pollute global namespace encapsulate instead
+//don't use var
+//use strong type checks (=== and !==)
+
+
+//FUNCTIONS
+//generally functions should do only one thing
+//Don't use more than 3 functions parameters
+//use default parameters whenever possible
+//Generaly return, same data type as received
+//use arrow funtions when they make code more reabadle
+
+//OOP
+//use ES6 classes
+//Encapsulate data don't motate it from outside class
+//implement method chaining
+//dont use arrow function as methods(in regular objects)
+
+//AVOID NESTED CODE
+//use early return (guard clauses)
+//use ternary (conditional) or logical operators instead of if
+//use multiple if instead of if/else-if
+//avoid for loops use array methods instead
+//avoid call-back based asynchronous APIs
+
+
+//ASYNCHRONOUS CODE
+//consume promisses with async/await for best readability
+//whenever possible, run promisses in parallel (promisse.all)
